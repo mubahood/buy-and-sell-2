6a47eb963f8be46b3e8e1cdcc70dac10ed35a623
Muhindo Mubaraka <mubs0x@gmail.com>
2022-01-27 13:40:37 +0300

    \App\Models\Country

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Category extends Model 
+class Category extends Model  
 {
 
     public function attributes()
