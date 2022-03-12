46b4221927f3abc415966e98deaf0cd36a95141c
Muhindo Mubaraka <muhindomubaraka@iut-dhaka.edu>
2022-03-13 00:45:52 +0300

    anjane

--- resources/views/dashboard/upload.blade.php
+++ resources/views/dashboard/upload.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="http://localhost:8888/buy-and-sell-2/api/products" enctype="multipart/form-data" class="adpost-form">
+<form method="POST" action="{{ url('api/products') }}" enctype="multipart/form-data" class="adpost-form">
     @csrf
     <div class="form-group"><label class="form-label">Product photos</label>
         <input type="file" id="input-b6a" name="images[]" required
