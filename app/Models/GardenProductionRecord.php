2c1c960b9f86bcf9e3daf253b6e73bdcb266bd36
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-05-25 00:50:17 +0300

    return $g->name;

--- app/Models/GardenProductionRecord.php
+++ app/Models/GardenProductionRecord.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class GardenProductionRecord extends Model
 {
     use HasFactory;
+
+
+    public function getGardenNameAttribute()
+    {
+        $g = Garden::find($this->garden_id);
+        if ($g == null) {
+            return "-";
+        }
+        return $g->name;
+    }
+
+    protected $appends = [
+        'garden_name',
+    ];
 }
