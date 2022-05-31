d0bdee9440ee52d05f9dc850a8d521d7eaf3627f
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-05-31 08:37:44 +0300

    romina

--- app/Http/Controllers/ApiUsersController.php
+++ app/Http/Controllers/ApiUsersController.php
@@ -136,6 +136,100 @@ class ApiUsersController
         $u->email = $u->email;
         $u->username = $u->email;
 
+
+    
+  
+        $u->name = (string) ($request->name ? $request->name : "");
+        $u->email = (string) ($request->email ? $request->email : "");
+        $u->gender = (string) ($request->gender ? $request->gender : "");
+        $u->date_of_birth = (string) ($request->date_of_birth ? $request->date_of_birth : "");
+        $u->number_of_dependants = (string) ($request->number_of_dependants ? $request->number_of_dependants : "");
+        $u->marital_status = (string) ($request->marital_status ? $request->marital_status : "");
+        $u->user_role = (string) ($request->user_role ? $request->user_role : "");
+        $u->experience = (string) ($request->experience ? $request->experience : "");
+        $u->production_scale = (string) ($request->production_scale ? $request->production_scale : "");
+        $u->access_to_credit = (string) ($request->access_to_credit ? $request->access_to_credit : "");
+        $u->district = (string) ($request->district ? $request->district : "");
+        
+
+        unset($u->password);
+        unset($u->status_comment);
+        unset($u->opening_hours);
+        unset($u->remember_token);
+        unset($u->cover_photo);
+        unset($u->youtube);
+        unset($u->last_seen);
+        unset($u->status);
+        unset($u->linkedin);
+        unset($u->facebook);
+
+        if (isset($_FILES)) {
+            if ($_FILES != null) {
+                if (count($_FILES) > 0) {
+
+                    if(isset($_FILES['profile_pic'])){
+                        if($_FILES['profile_pic']!=null){
+                            if(isset($_FILES['profile_pic']['tmp_name'])){
+                                $u->avatar = Utils::upload_file($_FILES['profile_pic']);
+                            };
+                        }
+                        unset($_FILES['audio']);
+                    }
+                }
+            }
+        }
+ 
+
+        $u->save();
+
+        return Utils::response([
+            'status' => 1,
+            'message' => "Profile updated successfully.",
+            'data' => $u
+        ]);
+    }
+
+    public function update_old(Request $request)
+    {
+
+        if (
+            $request->email == null ||
+            $request->name == null ||
+            $request->user_id == null
+        ) {
+            return Utils::response([
+                'status' => 0,
+                'message' => "You must provide Name, email and user id.",
+                'data' => null
+            ]);
+        }
+
+        $user_id = (int) ($request->user_id ? $request->user_id : 0);
+        $email = (string) ($request->email ? $request->email : "");
+        $u = User::find($user_id);
+        if ($u == null) {
+            return Utils::response([
+                'status' => 0,
+                'message' => "Failed to find account with ID {$user_id}",
+                'data' => null
+            ]);
+        }
+
+        $_u = User::where('email', $email)->get();
+        if (isset($_u['0'])) {
+            if ($_u['0']->id != $u->id) {
+                return Utils::response([
+                    'status' => 0,
+                    'message' => "Changes not saved because user with same email ({$user_id}) that you provided already exist.",
+                    'data' => null
+                ]);
+            }
+        }
+
+        $u->email = $email;
+        $u->email = $u->email;
+        $u->username = $u->email;
+
         $u->name = (string) ($request->name ? $request->name : "");
         $u->username = (string) ($request->email ? $request->email : "");
         $u->company_name = (string) ($request->company_name ? $request->company_name : "");
