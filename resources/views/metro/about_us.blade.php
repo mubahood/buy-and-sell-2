add57b1dbcc0218f1f0a769459b7b05eb8874a62
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-03 16:27:06 +0300

    romina

---
+++ resources/views/metro/about_us.blade.php
@@ -0,0 +1,15 @@
+@extends('metro.layout.layout-main')
+
+@section('main-content')
+    <div class="row mt-5 justify-content-center ">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam dicta magnam officia perspiciatis
+                    labore, ullam quam suscipit tempore iusto beatae excepturi. Aliquid, distinctio ratione. Nihil iusto
+                    veniam illo quo odio!
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
