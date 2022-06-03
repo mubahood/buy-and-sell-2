0e550fd88e6a0c48416f2d6b88bfbab14394689d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-06-04 00:32:05 +0300

    category_id

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends AdminController
      *
      * @var string
      */
-    protected $title = 'Product';
+    protected $title = 'Market place';
 
     /**
      * Make a grid builder.
@@ -26,26 +26,52 @@ class ProductController extends AdminController
     {
         $grid = new Grid(new Product());
 
-        $grid->column('id', __('Id'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-        $grid->column('name', __('Name'));
-        $grid->column('category_id', __('Category id'));
-        $grid->column('user_id', __('User id'));
-        $grid->column('country_id', __('Country id'));
-        $grid->column('city_id', __('City id'));
-        $grid->column('price', __('Price'));
-        $grid->column('slug', __('Slug'));
-        $grid->column('status', __('Status'));
-        $grid->column('description', __('Description'));
-        $grid->column('quantity', __('Quantity'));
-        $grid->column('images', __('Images'));
-        $grid->column('thumbnail', __('Thumbnail'));
-        $grid->column('attributes', __('Attributes'));
-        $grid->column('sub_category_id', __('Sub category id'));
-        $grid->column('fixed_price', __('Fixed price'));
+        //$grid->column('id', __('Id'));
+        //$grid->column('updated_at', __('Updated at'));
+        $grid->column('id', __('Photo'))->display(function ($id) {
+            return '<img width="40" src="' . $this->get_thumbnail() . '" ?>';
+        })->sortable();
+
+        $grid->column('name', __('Product'))->display(function ($id) {
+            return $this->get_name_short();
+        })->sortable();
+
+        $grid->column('price', __('Price'))->display(function ($category_id) {
+            return $this->price_text;
+        })->sortable();
+
         $grid->column('nature_of_offer', __('Nature of offer'));
 
+        $grid->column('user_id', __('Offer by'))->display(function () {
+            return $this->seller_name;
+        })->sortable();
+
+
+        $grid->column('category_id', __('Category'))->display(function ($category_id) {
+            if ($this->category == null) {
+                return $category_id;
+            }
+            return $this->category->name;
+        })->sortable(); 
+       
+        
+        $grid->column('city_id', __('Location'))
+            ->display(function ($city_id) {
+                if ($this->location == null) {
+                    return $city_id;
+                }
+                return $this->location->get_name();
+            })->sortable();
+
+
+        $grid->column('created_at', __('Posted'));
+
+        //$grid->column('sub_category_id', __('Sub category id'));
+        //$grid->column('fixed_price', __('Fixed price'));
+        //$grid->column('attributes', __('Attributes'));
+        //$grid->column('images', __('Images'));
+        //$grid->column('quantity', __('Quantity'));
+        //$grid->column('description', __('Description'));
         return $grid;
     }
 
