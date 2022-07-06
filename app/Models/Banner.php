b348fa9cd4f998d13a256738ba7366d84146dc05
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-07-07 01:24:21 +0300

    anjane

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+
+    public function getImageAttribute($img)
+    {
+        return 'storage/'.$img;
+    }
+
 }
