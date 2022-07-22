2133afd77f5f23ad8e183db5e2d082b56236664b
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-07-22 20:27:39 +0300

    cFqyb2oSD6efGch2PfWuI4FupPYkyvhrPJUTrhr4.png

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -228,7 +228,6 @@ class Utils
         }
 
         $path = Storage::putFile('/public/storage', $file['tmp_name']);
-        $path = str_replace('public/storage/','',$path);
         return $path;
     }
     public static function upload_images($files)
