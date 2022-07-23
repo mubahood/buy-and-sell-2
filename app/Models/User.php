edb9a3aec296fd4415acef486f04badf784aa8b7
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-07-23 11:51:38 +0300

    chat issue fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -127,7 +127,10 @@ class User extends Authenticatable
 
     public function getAvatarAttribute($avatar)
     {
-        //return $avatar;
+        if($avatar == null){
+            return url('no_image.jpg');
+        }
+        //return $avatar; 
         return url( $avatar);
     }
 
