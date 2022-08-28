8d736c6636f389422fb711a483b3aec41d197d53
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-28 20:36:06 +0300

    order

--- app/Http/Controllers/DashboardLocationControler.php
+++ app/Http/Controllers/DashboardLocationControler.php
@@ -71,7 +71,7 @@ class DashboardLocationControler extends Controller
 
             $item->photo = 'no_image.png';
             $item->details = '';
-            $item->order = '';
+            $item->order = 1;
 
             $item->save();
             return redirect('dashboard/locations');
