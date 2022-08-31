a38203e20a9286136d3ddfa9f73f43e6063d9dd2
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-31 10:20:33 +0300

    romina

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -31,4 +31,25 @@ class HomeController extends Controller
             //     });
             // });
     }
+    public function stats(Content $content)
+    {
+        return $content
+            ->title('Dashboard')
+            ->description('Description...');
+            //->row(Dashboard::title());
+            // ->row(function (Row $row) {
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::environment());
+            //     });
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::extensions());
+            //     });
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::dependencies());
+            //     });
+            // });
+    }
 }
